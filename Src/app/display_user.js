1976ce18f671b27b70d875706e678ea04c7a0871
Deepesh <deepesh.kt10636@gmail.com>
2019-11-02 21:26:45 +0530

    working on delete

--- Src/app/display_user.js
+++ Src/app/display_user.js
@@ -4,11 +4,13 @@
 
 function userList() 
 {
+    $("#output").empty();
     var userList = store.onGetUserFromList();
     console.log(userList);
    
     userList.forEach((user)=>
     {
+
         var divRow = $("<div>").addClass("row");        
         var divName = $("<div>").html(user.name).addClass("col-md-4");
         var divEmail = $("<div>").html(user.emailId).addClass("col-md-4"); 
